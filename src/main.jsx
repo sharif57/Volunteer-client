62cb7664c28033d22d0807bb6d7854a311081460
sharif57 <sharifmahamud577951@gmail.com>
2024-05-12 19:43:18 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home';
+import Layout from './layout/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
